33169aba96fc9379b3a5d69be71d6da91462a9d4
Prince Ajuobi <princekelsey299@gmail.com>
2019-05-10 16:52:13 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
-    filename: "js/bundle.js"
+    filename: "bundle.js"
   },
   devServer: {
     contentBase: "./dist"
