1cfa0d25a18942a3fa57b9283abc214bc79d98ae
Prince Ajuobi <princekelsey299@gmail.com>
2019-05-10 13:56:03 +0300

    server

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(dist));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(port);
